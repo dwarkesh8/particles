349c375b95ea9fb02adb24a701e29700b2acd36a
Mr. Dwarkesh Purohit <dwarkeshpurohit8@gmail.com>
2023-02-23 10:58:14 +0530

    update code to make it able to use externally
    added context, canvas and other references as args

--- classes.js
+++ classes.js
@@ -1,5 +1,6 @@
 class Particle {
 	constructor(x, y, r, vx, vy, pColor) {
+			// this.canvas = canvas;
 		this.x = x;
 		this.y = y;
 		this.r = r;
@@ -7,20 +8,21 @@ class Particle {
 		this.vx = vx;
 		this.vy = vy;
 		this.pColor = pColor;
-	}
-	draw() {
-		ctx.beginPath();
-		ctx.fillStyle = this.pColor;
-		ctx.arc(this.x, this.y, this.r, 0, 2*Math.PI);
-		ctx.fill();
 
-		ctx.fillStyle = 'black';
-		ctx.font = "15px Arial";
-		ctx.fillText('x: '+this.x.toFixed(3), canvas.width-100, 20); 
-		ctx.fillText('y: '+this.y.toFixed(3), canvas.width-100, 40); 
+	}
+	draw(context) {
+		context.beginPath();
+		context.fillStyle = this.pColor;
+		context.arc(this.x, this.y, this.r, 0, 2*Math.PI);
+		context.fill();
+
+			// context.fillStyle = 'black';
+			// context.font = "15px Arial";
+			// context.fillText('x: '+this.x.toFixed(3), this.canvas.width-100, 20); 
+			// context.fillText('y: '+this.y.toFixed(3), this.canvas.width-100, 40); 
 	}
 
-	update() {
+	update(canvas) {
 		if (this.x > canvas.width-this.r/2) {
 			this.vx = this.vx*-1;
 		}
@@ -38,5 +40,3 @@ class Particle {
 		this.y = this.y+this.vy;
 	}
 }
-let p1 = new Particle(200, 100, 5, 3, 6, 'RED');
-let p2 = new Particle(100, 200, 5, 6, 3, 'GREEN');
